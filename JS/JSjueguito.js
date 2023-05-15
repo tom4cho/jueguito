afae67698d010e6f7e33b4502533520f5fe7c8d4
Tomas Carmona <tomascduque@gmail.com>
2023-05-14 19:55:20 -0500

    verificacion de soporte de canvas

--- JS/JSjueguito.js
+++ JS/JSjueguito.js
@@ -1,6 +1,15 @@
 window.onload = function(){
-canvas = document.getElementById("canvas");
+    canvas = document.getElementById("canvas");
 
+    if(canvas && canvas.getContext){
+        ctx = canvas.getContext("2d");
+    }
+
+    if(ctx){
+        alert("su navegador soporta canvas")
+    }else{
+        alert("su navegador no soporta canvas")
+    }
 
 }
 
