3f198884aa8badb6c119773642942543ebc5f07f
Tomas Carmona <tomascduque@gmail.com>
2023-05-15 21:18:53 -0500

    añadiendo segundo cuadro

--- JS/JSjueguito.js
+++ JS/JSjueguito.js
@@ -6,12 +6,21 @@ window.onload = function(){
     }
 
     if(ctx){
-        //definimos colores y cuadrado
+        //primer cuadrado
         ctx.fillStyle = "yellow";
         ctx.strokeStyle = "#ff0000";
         ctx.lineWidth = 5;
         ctx.fillRect(50, 50, 100, 100);
         ctx.strokeRect(50, 50, 100, 100);
+
+        //segundo cuadrado
+        ctx.fillStyle = "rgb(255, 0, 169)";
+        ctx.strokeStyle = "#ff0000";
+        ctx.lineWidth = 5;
+        ctx.fillRect(250, 50, 100, 100);
+        ctx.strokeRect(250, 50, 100, 100);
+
+
     }else{
         alert("su navegador no soporta canvas")
     }
